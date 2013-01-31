8ac17dad021fbf8fe45efc26d24148ae322db6e8
Adrian Lee <jun.irok@gmail.com>
2013-01-31 18:04:34 -0500

    backup

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-(function($){
-  AppRouter = Backbone.Router.extend({
+var express = require('express'),
+    hbs = require('hbs'),
+    app = express();
 
-    routes:{
-      "": "home",
-      "!/register": "register",
-      "!/login": "login"
-    },
+////////////////////////////////////////////////
+// Express Configuration
+////////////////////////////////////////////////
+app.configure(function() {
+  app.set('port', process.argv[2] || process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hbs');
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'), { maxAge: 300000 });
+});
 
-    home: function() {
-      new HomeView();
-    },
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
 
-    register: function() {
-      new RegisterView();
-    },
+////////////////////////////////////////////////
+// Handlebars
+////////////////////////////////////////////////
+var blocks = {};
 
-    login: function() {
-      new LoginView();
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
     }
-  });
-}(jQuery));
+
+    block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    // clear the block
+    blocks[name] = [];
+    return val;
+});
+
+////////////////////////////////////////////////
+// Router
+////////////////////////////////////////////////
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+////////////////////////////////////////////////
+// HTTP Server
+////////////////////////////////////////////////
+app.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
---
+++ stackmob/app.js
@@ -0,0 +1,23 @@
+(function($){
+  AppRouter = Backbone.Router.extend({
+
+    routes:{
+      "": "home",
+      "!/register": "register",
+      "register": "register",
+      "!/login": "login"
+    },
+
+    home: function() {
+      new HomeView();
+    },
+
+    register: function() {
+      new RegisterView();
+    },
+
+    login: function() {
+      new LoginView();
+    }
+  });
+}(jQuery));
