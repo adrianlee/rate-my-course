3b65abe166358f7287d00cbf77ef0266463145b5
Adrian Lee <jun.irok@gmail.com>
2013-03-02 17:18:44 -0500

    Fix null issue for capitalization filter

--- public/app.js
+++ public/app.js
@@ -72,6 +72,10 @@ App.config(['$routeProvider', function($routeProvider) {
 
 App.filter('capitalize', function() {
   return function(input, scope) {
-    return input.substring(0,1).toUpperCase()+input.substring(1);
+    if (input){
+      return input.substring(0,1).toUpperCase()+input.substring(1);
+    } else {
+      return;
+    }
   }
 });
